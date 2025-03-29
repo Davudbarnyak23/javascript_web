1c9ba923a1c0cd80e2d3f65205f9330a4eb53e92
david <barnakdavid49@gmail.com>
2025-03-29 14:10:37 +0200

    ter

--- 03-functions-objects/main.js
+++ 03-functions-objects/main.js
@@ -1,19 +1,19 @@
 const products = [
-{ name: 'Daenerys Sitting on Throne', price: 512, img: 'daenerys.jpeg', type: 'publick' },
-{ name: 'Fawkes Vinyl Figure', price: 312, img: 'fawkes.jpeg', type: 'VIP' },
-{ name: 'Flash Vinyl Figure', price: 645, img: 'flash.jpeg', type: 'publick' },
-{ name: 'Kakashi', price: 700, img: 'kakashi.jpeg', type: 'VIP' },
-{ name: 'The Mandalorian on Blurg', price: 400, img: 'mandalorian.jpeg', type: 'publick' },
-{ name: 'Naruto', price: 639, img: 'naruto.jpeg', type: 'VIP' },
-{ name: 'Rey Vinyl Figure', price: 138, img: 'rey.jpeg', type: 'publick' },
-{ name: 'Steve Trevor Vinyl Figure', price: 138, img: 'steve.jpeg', type: 'publick' },
-{ name: 'Moana Te Ka', price: 211, img: 'te_ka.jpeg', type: 'publick' }
+    { name: 'Daenerys Sitting on Throne', price: 512, img: 'daenerys.jpeg', type: 'publick' },
+    { name: 'Fawkes Vinyl Figure', price: 312, img: 'fawkes.jpeg', type: 'VIP' },
+    { name: 'Flash Vinyl Figure', price: 645, img: 'flash.jpeg', type: 'publick' },
+    { name: 'Kakashi', price: 700, img: 'kakashi.jpeg', type: 'VIP' },
+    { name: 'The Mandalorian on Blurg', price: 400, img: 'mandalorian.jpeg', type: 'publick' },
+    { name: 'Naruto', price: 639, img: 'naruto.jpeg', type: 'VIP' },
+    { name: 'Rey Vinyl Figure', price: 138, img: 'rey.jpeg', type: 'publick' },
+    { name: 'Steve Trevor Vinyl Figure', price: 138, img: 'steve.jpeg', type: 'publick' },
+    { name: 'Moana Te Ka', price: 211, img: 'te_ka.jpeg', type: 'publick' }
 ];
 
 const users = [
-    {id:1, login: '1', password: '1', type:'publick'},
-    {id:2, login: '2', password: '2', type:'VIP'},
-    {id:3, login: '3', password: '3', type:'VIP'}
+    { id: 1, login: '1', password: '1', type: 'publick', sale: 4 },
+    { id: 2, login: '2', password: '2', type: 'VIP', sale: 20 },
+    { id: 3, login: '3', password: '3', type: 'VIP', sale: 20 }
 ]
 let userType = 'publick';
 let userLogin;
@@ -21,34 +21,51 @@ let userPassword;
 let user = {};
 
 function userVerification(login, password) {
-    let id = 0
-    for(let i = 0; i < users.length; i++) {
-        if (users[i].login === login && users[i]['password'] === password ){
-            return {login:users[i].login,
-                    type: users[i].type}
-        }
-    }
+    for (let i = 0; i < users.length; i++)
+        if (users[i].login === login && users[i]['password'] === password)
+            return users[i]
     return {}
 }
 
-if ( confirm('ви зареестрованій користувач?')){
-
+if (confirm('ви зареестрованій користувач?')) {
     userLogin = prompt('login ')
     userPassword = prompt('password ')
+    user = userVerification(userLogin, userPassword)            
+}
 
-    user = userVerification(userLogin, userPassword)
-    //if ('login' in user) {}
-    if(user.hasOwnProperty('login')) {
-        userType = user.type;
-    }
-}   
+console.log(user)
+
+function pDiv(img, name, price){
+    return "<div><img src='images/" 
+                + img + "'><p>" 
+                + name + "</p><p>" 
+                + Math.round(price) + "</p></div>";
+
+}
+
+function addProduct(product) {
+    if (user.hasOwnProperty('login')) {
+        if (user.type === "VIP") {
+            return pDiv(product.img, product.name, ((1-user.sale/100) * product.price))
+        } else if (product.type !== 'VIP')
+            return pDiv(product.img, product.name, ((1-user.sale/100) * product.price))
+    } else 
+        if (product.type !== 'VIP')
+            return pDiv(product.img, product.name, product.price);
+
+    return ''
+}
 
 console.log(userType)
 
-let content = "<div><header>Funka<h1></h1></header>"
+let content = "<div><header>Funka<h1></h1></header><main>"
+let div = ''
 
-//  TODO: TOVAR
+for (let i = 0; i < products.length; i++) {
+    console.log(div)
+    div += addProduct(products[i])
 
-content += "</div>"
+}
+content += div + "</main>"
 
-document.body.innerHTML = ContentVisibilityAutoStateChangeEvent;
+document.body.innerHTML = content;
