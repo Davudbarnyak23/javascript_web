b2bb9191b9e2e78388532913c98f25aa2c83a4d8
david <barnakdavid49@gmail.com>
2025-05-17 14:20:20 +0300

    12121212

--- 09-canvas/matrix.js
+++ 09-canvas/matrix.js
@@ -1,33 +1,47 @@
-const matrixBg = document.getElementById('matrix-bg')
+console.log('Start!');
 
-let ctx_bg = matrixBg.getContext()
+const matrixBg = document.getElementById('matrix-bg')
+console.log(matrixBg)
+
+let ctx_bg = matrixBg.getContext('2d');
 let cw = matrixBg.width 
 let ch = matrixBg.clientHeight
 let font = 10
 let col = cw / font
-let arrSymbol = []
+let arrSymbolPosY = []
 
-for(let i = 0; i < cor; i++){
-    arrSymbol[i] = 1;
+for(let i = 0; i < col; i++){
+    arrSymbolPosY[i] = 1;
 }
 
 
-const str = "укнв1а ге2ковегшсвол внш4рао аенлшвкеог пеноа ве9олншлш-вн рп=ваш"
+const str = "укjнв1а ге2кjовjег шсвол внш4рао ае7S нлшjв кеог пеT ноа ве9олншлш-вн рп=ваш"
 let matrixStrArr = str.split('')
 console.log(matrixStrArr)
 
+ctx_bg.fillStyle = "rgb(0, 0, 0)";
+ctx_bg.fillRect(0, 0, cw, ch);
+
 function drawSymbol(){
-    ctx_bg.fillStyle = "rgba(0, 0, 0, 0.05)";
-    ctx_bg.fillrect(0, 0, cw, ch);
+    ctx_bg.fillStyle = "rgba(20, 19, 19, 0.05)";
+    ctx_bg.fillRect(0, 0, cw, ch);
 
     ctx_bg.fillStyle = "#0f0";
     ctx_bg.font = font + "px system-ui";
 
-    for(let i = 0; i < Array.length; i++ ){
-        let s = matrixStrArr[Math.floor(Math.random() * matrixStrArr.length)]
+    for(let i = 0; i < arrSymbolPosY.length; i++ ){
+        let s = matrixStrArr[
+            Math.floor(Math.random() * matrixStrArr.length)
+
+        ]
+        ctx_bg.fillText(s, i * font, arrSymbolPosY[i] * font)
+
+        if(arrSymbolPosY[i] * font > ch && Math.random() > 0.95 )
+            arrSymbolPosY[i] = 0
+
+    arrSymbolPosY[i]++
+    //console.log(arrSymbolPosY)
     }
 }
-    ctx_bg.fillText(s, i * font, arrSymbol[i] * font)
-
-    arrSymbol[i]++
 setInterval(drawSymbol, 150)
+
---
+++ 10-class/ghost.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('ghost')
+const ctx = canvas.getContext('2d')
+const cWidth = canvas.width;
+const cHeight = canvas.height;
+console.log(canvas)
+const imgFloder = 'images/'
+
+const bgImg = new Image()
+bgImg.src = imgFloder + '??';
