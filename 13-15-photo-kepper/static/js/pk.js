8d431687051d629fda6e0bcd562009deb80f92da
david <barnakdavid49@gmail.com>
2025-09-06 14:15:14 +0300

    6767

--- 13-15-photo-kepper/static/js/pk.js
+++ 13-15-photo-kepper/static/js/pk.js
@@ -14,83 +14,111 @@ class MainMenu {
 }
 
 
-class PhotoGallery {
-    constructor () {
-    this.allComments = [
-    'Цей кадр нереально крутий! :)',
-    'Ти вмієш дивувати! Кожен кадр - поєднання життєлюбності і краси',
-    'Спинися мить, прекрасна ти!',
-    'Просто супер! Як тобі це вдається?',
-    'Це просто шедевр мистецтва',
-    'В цьому штучному світі так приємно знайти щось натуральне))',
-    'Клас!!!))',
-    'Нереально чудово!',
-    'А ти вмієш дивувати ;)',
-    'Це фото так і проситься в рамочку на стіну'
-];
-
-    this.allDescriptions = [
-    'Коли радості немає меж',
-    'Любов в кожному пікселі',
-    'Фото заряджене позитивом',
-    'Зловив дзен',
-    'Як мало потрібно для щастя',
-    'Знали б ви що в мене на умі! ;)',
-    'Show must go on',
-    'Good vibes only',
-    'My inspiration',
-    'On my way to paradise',
-    'Що це, якщо не любов? Х)'
-];
-        this.pictureDB = this.generatePicturesDB(25)
+class PhotoGallery{
+    constructor(){
+        this.allComments = [
+            'Цей кадр нереально крутий! :)',
+            'Ти вмієш дивувати! Кожен кадр - поєднання життєлюбності і краси',
+            'Спинися мить, прекрасна ти!',
+            'Просто супер! Як тобі це вдається?',
+            'Це просто шедевр мистецтва',
+            'В цьому штучному світі так приємно знайти щось натуральне))',
+            'Клас!!!))',
+            'Нереально чудово!',
+            'А ти вмієш дивувати ;)',
+            'Це фото так і проситься в рамочку на стіну'
+        ];
+        
+        this.allDescriptions = [
+            'Коли радості немає меж',
+            'Любов в кожному пікселі',
+            'Фото заряджене позитивом',
+            'Зловив дзен',
+            'Як мало потрібно для щастя',
+            'Знали б ви що в мене на умі! ;)',
+            'Show must go on',
+            'Good vibes only',
+            'My inspiration',
+            'On my way to paradise',
+            'Що це, якщо не любов? Х)'
+        ];
+        this.picturesDB = this.generatePicturesDB(25)
+        //console.log(this.picturesDB)
         this.pictureContainer = document.querySelector('.picturesContainer')
+        this.opendPhotoContainer = document.querySelector('.openedPhotoContainer')
+        this.initEventListeners()
     }
-randomElement(array){
-    return array[Math.floor(Math.random() * array.length)]
-}
+    randomElement(array){
+        return array[Math.floor(Math.random() * array.length)]
+    }
 
     generatePicturesDB(number){
-    const pictures = []
+        const pictures = []
+        
+        for (let i = 0; i < number; i++){
+            const comments = []
+            const commentCount = Math.floor(Math.random() * 5)
 
-    for (let i=0; i< number; i++){
-        const comments = []
-        const commentCount = Math.random(Math.random() * 10);
+            for (let j = 0; j < commentCount; j++)
+                comments.push(this.randomElement(this.allComments))
 
-        for (let j=0; j < commentCount; j++ );
-            comments.push(this.randomElement(this.allComments));
-    pictures.push({
-        src: `../static/img/photos/${i}.jpg`,
-        likes: Math.floor(Math.random() * 100),
-        effect: 'none',
-        description: this.randomElement(this.allDescriptions),
-        comments: comments,
-        commentsNumber: comments.length
-    })}
-
-    return pictures;
-    console.log(this.pictureDB)
+            pictures.push({
+                src: `../static/img/photos/${i}.jpg`,
+                likes: Math.floor(Math.random() * 100),
+                effect: 'none',
+                description: this.randomElement(this.allDescriptions),
+                comments: comments,
+                commentsNumber: comments.length
+            })
+        }
+        return pictures;
     }
-    showPictures() {
+    showPictures(){
         const pictureTemplate = document.getElementById('templatePictureExample')
-        const pictureExample = pictureTemplate.content.querySelector('.pictureExample')
-        this.pictureContainer.innerHTML = '';
 
-        this.pictureDB.forEach((photo) => {
+        const pictureExample = pictureTemplate.content.querySelector('.pictureExample')
+        this.pictureContainer.innerText = ''
+
+        this.picturesDB.forEach( (photo) => {
             const photoBlock = pictureExample.cloneNode(true)
             photoBlock.querySelector('.pictureImg').src = photo.src;
             photoBlock.querySelector('.pictureImg').style.filter = photo.effect
             photoBlock.querySelector('.pictureComments').innerText = photo.commentsNumber
-            photoBlock.querySelector('.pictureStars').innerText = photo.likes 
+            photoBlock.querySelector('.pictureStars').innerText = photo.likes
 
-            this.pictureContainer.append(photoBlock)
-            
+            this. pictureContainer.append(photoBlock)
+
         })
-        //console.log(pictureExample)
+        // console.log(pictureExample)
+    }
+
+    showCheckedPictures(){
+        this.opendPhotoContainer.querySelector('.openedPhotoImg').src = picture.src
+        this.opendPhotoContainer.querySelector('.openedPhotoImg').style.filter = picture.effect
+        this.opendPhotoContainer.querySelector('.likesCount').innerText = picture.likes
+        this.opendPhotoContainer.querySelector('.descriptionText').innerText = picture.description
+    }
+    initEventListeners(){
+        this.pictureContainer.addEventListener('click', (e) => {
+            if (e.target.classList.contains('pictureImg')){
+                //this.openPhoto(e.target);
+                const src = e.target.src
+                const picture = this.picturesDB.find( (pic) => pic.src === src)
+                if (picture){
+                    this.showCheckedPictures(picture);
+                }
+            }
+        });
+
+        this.cloneButton.addEventListener('click', (e) => {
+            this.opendPhotoContainer.classList.add('hidden');
+            this.opendPhotoContainer.innerText = '';
+        })
     }
 }
 
-document.addEventListener('DOMContentLoaded',()=> {
+document.addEventListener('DOMContentLoaded', ()=>{
     new MainMenu('.menuTrigger', '.mainMenuContainer')
-    const gallery = new PhotoGallery();
+    const gallery = new PhotoGallery
     gallery.showPictures();
-} )
+})
